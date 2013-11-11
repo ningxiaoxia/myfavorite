005fa8356eb2a3e2ffbf0069253144e74e7b555a
ningixaoxia <2443066491@qq.com>
2013-11-11 08:19:47 +0800

    Signed-off-by: ningixaoxia <2443066491@qq.com>

---
+++ js/console.js
@@ -0,0 +1,11 @@
+if (window.console) {
+    var cons = console;
+    if (cons) {
+    	cons.log("晓霞，能够遇到你，真的很好。\n");
+    	cons.log("我不会忘记当初的诺言！\n");
+    	cons.log("可以遇到自己喜欢的那个人，真的不容易，所以我会好好把握。\n");
+    	cons.log("%c❤ ❤\n","color:rgb(246,105,147);font-weight:bold;");
+    	cons.log("邓志明 673683680 dandzm@sina.com")
+    	
+    }
+}
